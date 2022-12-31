e06ffd483d6d5c10d4491a4ad2e4c0ad9b99cdba
raddick <raddick@jhu.edu>
2022-12-31 07:13:40 -0500

    in process of adding sdss3 or sdss4 or sdss5 publications display

--- publications.php
+++ publications.php
@@ -1,9 +1,5 @@
 <?php
-// publications array keys:
-//   Array ( [0] => adsabs [1] => adsabs_url [2] => arxiv [3] => arxiv_url [4] => author_alphabetic [5] => authors [6] => blind_analysis [7] => category [8] => doi [9] => doi_url [10] => forward_url [11] => identifier [12] => journal [13] => journal_page [14] => journal_reference [15] => journal_volume [16] => journal_year [17] => modified [18] => paper_id [19] => paper_url [20] => publication_id [21] => reference [22] => status [23] => survey [24] => title )
 
-// categories:
-// Array ( [0] => Infrastructure Paper [1] => Meeting Abstract [2] => Paper in Conference Proceeding [3] => Scientific Paper in Journal [4] => Technical Paper in Journal )
 
 function show_publications($atts) {
 
@@ -11,11 +7,21 @@ function show_publications($atts) {
 
     $wporg_atts = shortcode_atts(
 		array(
+            'survey' => 'sdss5',
 			'type' => ''
 		), $atts
 	);
 
-    $publications_data_json = @file_get_contents(  PATH_JSON . 'publications.json' );
+    if ($wporg_atts['survey'] == 'sdss5') {
+        $publications_data_json = @file_get_contents(  PATH_JSON . 'publications.json' );
+    }
+    elseif (($wporg_atts['survey'] == 'sdss4') || ($wporg_atts['survey'] == 'sdss3')) {
+        $publications_data_json = @file_get_contents(  PATH_JSON_SDSS4 . 'publications.json' );
+    } else {
+        $thehtml = '<h2><font color="red">ERROR: Survey '.$wporg_atts['survey'].' not found!</font></h2>';
+        return $thehtml;
+    }
+
     $publications_data = json_decode( $publications_data_json, true );
 
     $thehtml = "";
@@ -26,6 +32,14 @@ function show_publications($atts) {
     } else {
         $these_publications = $publications_data['publications'];
     }
+
+    if ($wporg_atts['survey'] == 'sdss4') {
+        $these_publications = array_filter($publications_data['publications'], "is_sdss4");
+    } elseif ($wporg_atts['survey'] == 'sdss3') {
+        $these_publications = array_filter($publications_data['publications'], "is_sdss3");
+    } else {
+        $these_publications = $publications_data['publications'];
+    }
     
     foreach ($these_publications as $this_pub):
         $dflt_url = ( !empty( $this_pub[ 'adsabs_url' ] ) ) ? $this_pub[ 'adsabs_url' ] : 
@@ -33,7 +47,7 @@ function show_publications($atts) {
     					(( !empty( $this_pub[ 'arxiv_url' ] ) ) ? $this_pub[ 'arxiv_url' ] : false )
                     );
 
-        $thehtml .= "<li>"; // $thehtml .= "<li><i class='fa-li fa fa-book'></i>";   // when we get font awesome glyphs to work
+        $thehtml .= "<li>".$this_pub['survey'].": "; // $thehtml .= "<li><i class='fa-li fa fa-book'></i>";   // when we get font awesome glyphs to work
 
         if ( $dflt_url ) $thehtml .= "<a target='_blank' href='$dflt_url' >";
         $thehtml .= "<strong>" . $this_pub[ 'title' ] . "</strong>";
@@ -64,5 +78,76 @@ function is_tech_paper($var) {
     }
 }
 
+function is_sdss4($var) {
+    $sdss4_surveys = array('APOGEE-2', 'MaNGA', 'MaStar', 'SDSS-IV General', 'SPIDERS', 'TDSS', 'eBOSS');
+    if (in_array($var['survey'], $sdss4_surveys)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function is_sdss3($var) {
+    $sdss3_surveys = array('APOGEE', 'BOSS', 'MARVELS');
+    if (in_array($var['survey'], $sdss3_surveys)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// sdss5 publications array keys:
+//   Array ( [0] => adsabs [1] => adsabs_url [2] => arxiv [3] => arxiv_url [4] => author_alphabetic [5] => authors [6] => blind_analysis [7] => category [8] => doi [9] => doi_url [10] => forward_url [11] => identifier [12] => journal [13] => journal_page [14] => journal_reference [15] => journal_volume [16] => journal_year [17] => modified [18] => paper_id [19] => paper_url [20] => publication_id [21] => reference [22] => status [23] => survey [24] => title )
+
+// sdss4 publications array keys:
+//Array ( [0] => adsabs [1] => adsabs_url [2] => arxiv [3] => arxiv_url [4] => author_alphabetic [5] => authors [6] => blind_analysis [7] => category [8] => doi [9] => doi_url [10] => first_author [11] => identifier [12] => journal [13] => journal_page [14] => journal_reference [15] => journal_volume [16] => journal_year [17] => member_ids [18] => modified [19] => publication_id [20] => status [21] => survey [22] => title )
+
+
+// sdss5 category values:
+// Array ( [0] => Infrastructure Paper [1] => Meeting Abstract [2] => Paper in Conference Proceeding [3] => Scientific Paper in Journal [4] => Technical Paper in Journal )
+
+// sdss4 category values:
+// Array ( [0] => Conference Proceeding based on Public SDSS Data [1] => Data Release Paper [2] => Meeting Abstract [3] => Paper in Conference Proceeding [4] => Scientific Paper in Journal [5] => Scientific Paper in Journal based on Public SDSS Data [6] => Technical Paper in Journal )
+
+// sdss4 survey valeus:
+// Array ( [0] => APOGEE [1] => APOGEE-2 [2] => BOSS [3] => MARVELS [4] => MaNGA [5] => MaStar [6] => SDSS-IV General [7] => SPIDERS [8] => TDSS [9] => eBOSS )
+
+
+/*  TO FIND KEYS:
+    $keylist = array();
+    foreach ($publications_data['publications'] as $x) {
+        foreach ($x as $k => $v) {
+            if (!in_array($k, $keylist)) {
+                array_push($keylist, $k);
+            }
+        }
+    }
+    sort($keylist);
+    print_r($keylist);
+
+    */
+
+/*  TO FIND CATEGORIES:
+    $catlist = array();
+    foreach ($publications_data['publications'] as $x) {
+        if (!in_array($x['category'], $catlist)) {
+            array_push($catlist, $x['category']);
+        }
+    }
+    sort($catlist);
+    print_r($catlist);
+*/
+
+/*  TO FIND SURVEYS:
+    $surveylist = array();
+    foreach ($publications_data['publications'] as $x) {
+        if (!in_array($x['survey'], $surveylist)) {
+            array_push($surveylist, $x['survey']);
+        }
+    }
+    sort($surveylist);
+    print_r($surveylist);
+*/
 
 ?>
